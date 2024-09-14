b1cf90ac90a76103a86e78378ad211a72a904313
aabbhhiijadhav <abhijadhav0216@gmail.com>
2024-09-14 12:30:45 +0530

    Add a three fragment 1.For home page 2.For category page 3.For My Order Page

--- app/src/main/java/com/example/foodiz/HomePage.java
+++ app/src/main/java/com/example/foodiz/HomePage.java
@@ -2,6 +2,7 @@ package com.example.foodiz;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
 import android.view.MenuItem;
@@ -22,19 +23,48 @@ public class HomePage extends AppCompatActivity implements BottomNavigationView.
         bottomNavigationView=findViewById(R.id.home_page_bottomnavigationview);
         bottomNavigationView.setOnNavigationItemSelectedListener(this);
 
-        //this metod is used for when we will open our application that time which is our default bottom navigation menu and our default navigation fragment.
+        //this method is used for when we will open our application that time which is our default bottom navigation menu and our default navigation fragment.
         bottomNavigationView.setSelectedItemId(R.id.navigation_menu_Home);
+
 
 
 
 
     }
 
+
+    //when we create a fragment in our application before using it first initialize that fragment.
+    //initialization of home fragment
+    HomeFragment homeFragment = new HomeFragment();
+
+    //initialization of category fragment
+    CategoryFragment categoryFragment=new CategoryFragment();
+
+    //initialization of myorder fragment
+    MyorderFragment myorderFragment = new MyorderFragment();
+
+
     //this method is compalsary because when we click on any item of navigation item
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        //decision when we are click on Home button
+        if (item.getItemId() == R.id.navigation_menu_Home){
+            //this process is used for load the fragment on the our framnr layout
+
+            //use of all methods
+            //getSupportFragementManager = this method is used to load the fragment
+            //beginTransaction() = this method is used for top start the load the fragment in the framelayout
+            //replace = replace method is used to replace a new fragment on the place present fragment6
+            getSupportFragmentManager().beginTransaction().replace(R.id.homeframelayout,homeFragment).commit();
+
+        } else if (item.getItemId() == R.id.navigation_menu_category) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.homeframelayout,categoryFragment).commit();
+        }
+        else if(item.getItemId() == R.id.navigation_menu_myorder) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.homeframelayout,myorderFragment).commit();
+        }
 
 
-        return false;
+        return true;
     }
 }
